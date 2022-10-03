f8e3ae7f6b384e63cbe7b7c64e4fb1fbb232cccb
Eve <evelinheks@gmail.com>
2022-10-03 13:34:34 -0300

    consumindo dados eleicao 2022

--- presidente.py
+++ presidente.py
@@ -1,10 +1,16 @@
 import requests
 import json
 
+
 def buscar_dados():
-    request = requests.get("https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json")
-    todos = json.loads(request.content);
-    valor =todos["cand"];
+    request = requests.get(
+        "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json")
+    todos = json.loads(request.content)
+    valor = todos["cand"]
     for item in valor:
-      print(item)
-buscar_dados() 
+        print(item['nm'])
+        print("Votos:"+item['vap'])
+        print(item['pvap']+"%\n")
+
+
+buscar_dados()
